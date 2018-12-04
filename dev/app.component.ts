66ef33afceba0ccc5d3d9b8cd8892e5c6852f99a
jaguso <jagutzo@gmail.com>
2018-12-04 01:55:03 -0600

    made data show up when you click on name

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -2,11 +2,27 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+    template: ` 
+        <h3 
+        (click)="onSelect()"
+        >{{contact.firstName}} {{contact.lastName}}</h3>
+        <div *ngIf="showDetail === true">
+            Phone Number: {{contact.phone}}<br>
+            Email: {{contact.email}}
+        </div>        
     `,
 })
 export class AppComponent {
+    public contact = {
+        firstName: "Jaime",
+        lastName: "Garcia",
+        phone: "345920349",
+        email: "jj@gmail.com"
+    };
 
+    public showDetail = false;
+
+    onSelect() {
+        this.showDetail = true;
+    }
 }
