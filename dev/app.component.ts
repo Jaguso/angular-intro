aefb49624660a1aaddf476156222c03203d99d9f
jaguso <jagutzo@gmail.com>
2018-12-04 01:59:47 -0600

    name changes to red when clicked on it

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -5,12 +5,14 @@ import {Component} from 'angular2/core';
     template: ` 
         <h3 
         (click)="onSelect()"
+        [class.clicked]="showDetail === true" 
         >{{contact.firstName}} {{contact.lastName}}</h3>
         <div *ngIf="showDetail === true">
             Phone Number: {{contact.phone}}<br>
             Email: {{contact.email}}
         </div>        
     `,
+    styleUrls: ["../src/css/app.css"]
 })
 export class AppComponent {
     public contact = {
